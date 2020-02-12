f93527b23a6c2f638523970087129ad5e071a009
Brian Prickril <bprickril22@gmail.com>
2020-02-12 11:14:22 -0500

    added delete but app isn't starting

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ app.use(express.json());
 
 app.get("*", function(req, res){
     res.sendFile(path.join(publicDir, "index.html"));
-});
+})
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(publicDir, "notes.html"));
-});
+})
 
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
-});
+})
 
 app.get("/api/notes/:id", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
-});
+})
 
 app.post("/api/notes", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -37,14 +37,27 @@ app.post("/api/notes", function(req, res) {
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
     console.log("Successfully saved note:", newNote);
     res.json(savedNotes);
-});
+})
 
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleted: ${noteID}`);
+    savedNotes = savedNotes.filter(dNote => {
+        return dNote.id != noteID;
+    })
+    
+    for (dNote of savedNotes) {
+        dNote.id = newID.toString();
+        newID++;
+    }
 
-
-
-
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
 
 app.listen(port, function() {
-    console.log(`Listening on localhost:${port}`);
+    console.log(`Listening on http://localhost:${port}`);
 });
 
